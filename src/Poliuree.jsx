2bb57fbb7bc130ae23c752e45f22843c8b64dff3
LeonardoCahuas <leocahuas99@gmail.com>
2023-10-01 23:20:33 +0200

    poliuree

--- src/Poliuree.jsx
+++ src/Poliuree.jsx
@@ -1,12 +1,14 @@
 import BottomFooter from "./BottomFooter"
 import MidPoli from "./MidPoli"
 import Header from "./PoliHeader"
+import ThreePar from "./ThreePar"
 import TopFooter from "./TopFooter"
 
 function Poliuree(){
     return(
         <div>
             <Header/>
+            <ThreePar/>
             <MidPoli/>
             <TopFooter/>
             <BottomFooter/>
--- src/ThreePar.jsx
+++ src/ThreePar.jsx
@@ -1,7 +1,24 @@
-function ThreePar(){
-    return(
-        <div>
-
+function ThreePar() {
+    return (
+        <div className="w-100 three-wrap">
+            <h3 className="w-100 text-center three-title">Lorem ipsum dolor</h3>
+            <div className="d-flex flex-row three-cont">
+            <div className="d-flex flex-column align-items-center threecard">
+                    <i class="fa-solid fa-x"></i>
+                    <h4>Lorem ipsum dolor</h4>
+                    <p className="three-p">Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d   </p>
+                </div>
+                <div className="d-flex flex-column align-items-center threecard three-center">
+                    <i class="fa-solid fa-x"></i>
+                    <h4>Lorem ipsum dolor</h4>
+                    <p className="three-p">Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d   </p>
+                </div>
+                <div className="d-flex flex-column align-items-center threecard">
+                    <i class="fa-solid fa-x"></i>
+                    <h4>Lorem ipsum dolor</h4>
+                    <p className="three-p">Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d   </p>
+                </div>
+            </div>
         </div>
     )
 }
