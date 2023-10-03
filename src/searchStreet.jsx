225189b94716366928d4b6d927e08137015cc79a
mattia <matti.ledda@tiscali.it>
2023-10-04 00:31:40 +0200

    maps

--- src/searchStreet.jsx
+++ src/searchStreet.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import * as turf from '@turf/turf';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2bGVvIiwiYSI6ImNsbjdtcDNibzB6NW0ydG1ua2x0Y2I4cHkifQ.YJDbsqfZnH5XzUqa5mXf8w'; // Sostituisci con il tuo access token
 
 function SearchStreet() {
   const [searchQuery, setSearchQuery] = useState('');
   const [map, setMap] = useState(null);
+  const [area, setArea] = useState(null);
 
   // Funzione per cercare una via
   const searchStreet = () => {
@@ -34,8 +38,35 @@ function SearchStreet() {
       center: [-74.006, 40.7128], // Centro della mappa
       zoom: 10, // Zoom iniziale
     });
+
+    const draw = new MapboxDraw({
+      displayControlsDefault: false,
+      // Select which mapbox-gl-draw control buttons to add to the map.
+      controls: {
+        polygon: true,
+        trash: true
+      },
+      // Set mapbox-gl-draw to draw by default.
+      // The user does not have to click the polygon control button first.
+      defaultMode: 'draw_polygon'
+    });
+
+    newMap.addControl(draw);
+
+    newMap.on('draw.create', updateArea);
+    newMap.on('draw.delete', updateArea);
+    newMap.on('draw.update', updateArea);
+
     setMap(newMap);
   };
+
+  function updateArea(e) {
+    const data = e.features[0];
+    const area = turf.area(data);
+    // Restrict the area to 2 decimal points.
+    const rounded_area = Math.round(area * 100) / 100;
+    setArea(rounded_area);
+  }
 
   // Effettua l'inizializzazione della mappa quando il componente viene montato
   React.useEffect(() => {
@@ -55,6 +86,11 @@ function SearchStreet() {
         <button onClick={searchStreet}>Cerca</button>
       </div>
       <div id="map" className="map"></div>
+      {area && (
+        <div>
+          <p>Area del poligono: {area} metri quadrati</p>
+        </div>
+      )}
     </div>
   );
 }
