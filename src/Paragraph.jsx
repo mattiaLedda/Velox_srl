7446dd8e27ce4bf1f8ea84441889ca286ebf4d99
LeonardoCahuas <leocahuas99@gmail.com>
2023-10-15 15:32:19 +0200

    paragraph

--- src/Paragraph.jsx
+++ src/Paragraph.jsx
@@ -1,7 +1,12 @@
-function Paragraph(){
-    return(
-        <div>
-            
+function Paragraph(props) {
+    return (
+        <div className="d-flex align-items-center justify-content-center m-5">
+            <div className="text-center w-75">
+                <h3 className="fs-1">{props.title}</h3>
+                <p className="text-start fs-1">{props.text}</p>
+            </div>
         </div>
     )
 }
+
+export default Paragraph;
--- src/Preventivo.jsx
+++ src/Preventivo.jsx
@@ -10,6 +10,8 @@ import SearchStreet from "./searchStreet"
 import Form from "./Form"
 import StepperProva from "./StepperProva"
 import MapComponent from "./Maps2"
+import Paragraph from "./Paragraph"
+import paragraphs from "../paragraphs"
 
 function Preventivo(){
     return(
@@ -21,6 +23,7 @@ function Preventivo(){
             <Lines/>
             <HalfPicture/>
             <Divided/>
+            <Paragraph title={paragraphs.preventivo.title} text={paragraphs.preventivo.text}/>
             <TopFooter/>
             <BottomFooter/>
         </div>
