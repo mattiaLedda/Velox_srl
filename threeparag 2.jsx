cedb4c923f9b71f82cf8f414a21dfe1c2c05a5a5
LeonardoCahuas <leocahuas99@gmail.com>
2024-02-17 23:39:31 +0100

    last last

--- threeparag 2.jsx
+++
@@ -1,9 +0,0 @@
-const threeparag = {
-    first: {
-        icon : "fa-solid fa-x",
-        title: "Lorem ipsum dolor",
-        text:"Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced docet rocet lorem ispum gocet Lorem isum dolor lorem ipsum dolor ciao oorems come ispum solod rosce amorl dolor ipsum soler omet omelette ciao lorem ipsum dolor soced d  "
-    }
-}
-
-export default threeparag
